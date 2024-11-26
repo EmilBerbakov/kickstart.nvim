327d540960f67e492715db6c8da01b23fef2c390
Emil Berbakov <emberbakov@gmail.com>
2024-11-25 23:25:44 -0600

    I would like to have some lsps please

--- init.lua
+++ init.lua
@@ -616,6 +616,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
+        angularls = {},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
@@ -625,7 +626,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
         --
 
         lua_ls = {
