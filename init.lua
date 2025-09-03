56205b352c0e8e51a9b54c80e7e7a77d213bd353
Emil Berbakov <emil.berbakov@dciinc.org>
2025-09-02 21:33:07 -0400

    add some keymaps to telescope

--- init.lua
+++ init.lua
@@ -419,12 +419,19 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {}
+        defaults = {
+          mappings = {
+            i = {
+              ['<c-enter>'] = 'to_fuzzy_refine',
+              ['<C-f>'] = require('telescope.actions.layout').toggle_preview,
+            },
+          },
+        },
+        pickers = {
+          find_files = {
+            previewer = false,
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
