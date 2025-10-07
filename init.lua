ea5a8e1d6b10620897cd2fc932444a68d313f1f2
Emil Berbakov <emil.berbakov@dciinc.org>
2025-10-07 16:11:13 -0400

    lazy load more theme options

--- init.lua
+++ init.lua
@@ -889,6 +889,9 @@ require('lazy').setup({
     end,
   },
 
+  { 'folke/tokyonight.nvim', lazy = true },
+  { 'ellisonleao/gruvbox.nvim', lazy = true },
+
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
